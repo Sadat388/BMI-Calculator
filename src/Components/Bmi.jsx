9846549bc3102863f75c0f9a660317eb3a3a8710
Sadat388 <sadathossain388@gmail.com>
2025-02-07 05:40:33 +0600

    added JS logic and worked on styles

--- src/Components/Bmi.jsx
+++ src/Components/Bmi.jsx
@@ -1,19 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Bmi = () => {
-  return (
-    <div className="containerBmi">
-      <div className="wei-hei">
-        <div className="weight">
-          <span>Weight: </span>
-          <input type="text" placeholder="Enter your weight (KG)" />
-        </div>
+  const [weight, setWeight] = useState("");
+  const [height, setHeight] = useState("");
+  const [result, setResult] = useState(null);
 
-        <div className="height">
-          <span>Height: </span>
-          <input type="text" placeholder="Enter your height (meter)" />
+  const handleClick = () => {
+    const weightValue = parseFloat(weight);
+    const heightValue = parseFloat(height);
+
+    if (!isNaN(weightValue) && !isNaN(heightValue)) {
+      setResult(weightValue / (heightValue * heightValue));
+    } else {
+      alert("Please enter valid numbers!");
+    }
+  };
+
+  const handleClickClear = () => {
+    setWeight("");
+    setHeight("");
+    setResult(null);
+  };
+
+  return (
+    <>
+      <div className="title">
+        <h1>BMI CALCULATOR</h1>
+      </div>
+
+      <div className="containerBmi">
+        <div className="wei-hei">
+          <div className="weight">
+            <span>Weight: </span>
+            <input
+              type="text"
+              value={weight}
+              onChange={(event) => setWeight(event.target.value)}
+              placeholder="Enter your weight (KG)"
+            />
+          </div>
+
+          <div className="height">
+            <span>Height: </span>
+            <input
+              type="text"
+              value={height}
+              onChange={(event) => setHeight(event.target.value)}
+              placeholder="Enter your height (meter)"
+            />
+          </div>
+
+          <div className="bmi-btn">
+            <button class="btn btn-warning" onClick={handleClick}>
+              Calculate
+            </button>
+
+            <button class="btn btn-danger" onClick={handleClickClear}>
+              Clear
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+
+      <div className="bmi-result">
+        Your BMI: {result !== null ? result.toFixed(2) : ""}
+      </div>
+    </>
   );
 };
